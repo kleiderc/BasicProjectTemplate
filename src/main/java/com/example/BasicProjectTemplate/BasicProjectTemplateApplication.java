6d92384229b774f7182ec2ed468492aa9278bff2
kleiderc <kleiderc@gmail.com>
2025-02-18 18:14:32 -0400

    initial commit of full repository

---
+++ src/main/java/com/example/BasicProjectTemplate/BasicProjectTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.BasicProjectTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicProjectTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicProjectTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BasicProjectTemplate/BasicProjectTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BasicProjectTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicProjectTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
